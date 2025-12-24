31ce4522d8b9a601e9c68543032966dff5b5ed34
Elvis Gonçalves de Siqueira <167241022+ellgos@users.noreply.github.com>
2025-12-24 04:53:14 -0300

    Add game and player information to 2-tipos.py

---
+++ exercises/Python I - Introdução ao Python/2-tipos.py
@@ -0,0 +1,29 @@
+#Game information
+name = "Resident Evil 4 Remake"
+yearLaunch = 2023
+genre = "Survival Horror"
+price = 249.99
+print("Game:", name)
+print("Year of launch:", yearLaunch)
+print("Genre:", genre)
+print("Price:", price)
+print("")
+print(type(name))
+print(type(yearLaunch))
+print(type(genre))
+print(type(price))
+print("")
+
+
+#Player information
+playerName = "Leon S. Kennedy"
+playerLevel = 15
+isAlive = True
+print("Player Name:", playerName)
+print("Player Level:", playerLevel)
+print("Is Alive:", isAlive)
+print("")
+print(type(playerName))
+print(type(playerLevel))
+print(type(isAlive))
+print("")
